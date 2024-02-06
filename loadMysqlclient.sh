21a3c425e3f8cfbf01e137b7a3a04637b127670c
Resensys <57818865+twade12@users.noreply.github.com>
2024-02-06 10:38:44 -0500

    Delete loadMysqlclient.sh

--- loadMysqlclient.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-tee /etc/apt/sources.list.d/xenial.list > /dev/null <<EOF 
-deb http://dk.archive.ubuntu.com/ubuntu/ xenial main 
-deb http://dk.archive.ubuntu.com/ubuntu/ xenial universe 
-deb http://dk.archive.ubuntu.com/ubuntu/ xenial-updates universe 
-EOF
-
-apt-get update && apt-get install -y libmysqlclient20 libmysqlclient-dev && rm -rf /var/lib/apt/lists/*
